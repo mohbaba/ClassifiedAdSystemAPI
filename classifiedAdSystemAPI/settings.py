1b102cfb0cbe6a6b70d78dea19307c6efeb5ce93
banjoSolomon <ayomidebanjo68@gmail.com>
2024-06-18 23:45:25 +0100

    update

--- classifiedAdSystemAPI/settings.py
+++ classifiedAdSystemAPI/settings.py
@@ -127,3 +127,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    'users.auth_backend.MyAuthBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
